9178146fa2fad20efe4e6d25c60fce3fe4da2d00
Dave <lobster.spam.acct@gmail.com>
2018-06-30 20:17:32 +0100

    init

---
+++ src/main/java/uk/co/bigredlobster/gsmessagingstompwebsocket/Greeting.java
@@ -0,0 +1,18 @@
+package uk.co.bigredlobster.gsmessagingstompwebsocket;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
---
+++ src/main/java/uk/co/bigredlobster/gsmessagingstompwebsocket/HelloMessage.java
@@ -0,0 +1,21 @@
+package uk.co.bigredlobster.gsmessagingstompwebsocket;
+
+public class HelloMessage {
+
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
